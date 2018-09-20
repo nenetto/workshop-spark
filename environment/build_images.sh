136746812b8a144c2acb8750a0afa7b87f424e43
nenetto <nenetto@gmail.com>
2018-09-20 09:19:43 +0200

    Change tag for docker hub

--- environment/build_images.sh
+++ environment/build_images.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 PROJECT_VERSION=0.1.0-SNAPSHOT
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++"
+echo "Building images     "
+echo "+++++++++++++++++++++++++++++++++++++++++++++"
+
+
 #base
 docker build -t ubuntu-base base/base/
 
 #spark
-docker build -t flyday-spark base/spark/
+docker build -t nenetto/workshop-spark-master base/spark/
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "all images were build succesfully!     "
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++"
+echo "Pushing images    "
+echo "+++++++++++++++++++++++++++++++++++++++++++++"
+
+docker push nenetto/workshop-spark-master
+
+
 exit 0
